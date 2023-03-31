ff35bd6832e5aacb246d7620d3df34973fd7af99
“rohithgopala44” <rohithgopala44@gmail.com>
2023-03-31 21:47:27 +0530

    Added: Program Of Frequency Of Words By Using Java

---
+++ FrequencyOfWord.java
@@ -0,0 +1,45 @@
+package com.binarySearchTree;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import static java.util.Arrays.asList;
+public class FrequencyOfWord {
+
+        public void wordFrequency(String text) {
+
+            Map<String, Integer> freqMap = new HashMap<>();
+            asList(text.split(" ")).forEach(s -> {
+                if (freqMap.containsKey(s)) {
+                    Integer count = freqMap.get(s);
+                    freqMap.put(s, count + 1);
+                } else {
+                    freqMap.put(s, 1);
+                }
+            });
+            System.out.println(freqMap.toString());
+        }
+        public void specificWordFrequency(String text) {
+
+            Map<String, Integer> freqMap = new HashMap<>();
+            Pattern pattern = Pattern.compile("(?i)para");
+            Matcher matcher = pattern.matcher(text);
+            int count=0;
+            while (matcher.find()){
+                count++;
+            }
+            freqMap.put("para or Para", count);
+            System.out.println(freqMap.toString());
+        }
+
+        public static void main(String[] args) {
+            System.out.println("word frequency:");
+            FrequencyOfWord wordFrequency = new FrequencyOfWord();
+            String text = "To be or not to be";
+            String text2 = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            wordFrequency.wordFrequency(text);
+            wordFrequency.specificWordFrequency(text2);
+        }
+    }
+
